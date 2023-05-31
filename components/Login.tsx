a5e9a3845f379562d0b044f3b53a10a0a68c3c3f
“Pradaap-SS” <pradaapss@gmail.com>
2023-05-31 01:27:25 -0500

    #4  Implementing session & client providers, login

---
+++ components/Login.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
+function Login() {
+  return (
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+        <Image
+            src = "https://links.papareact.com/2i6"
+            width={300}
+            height={300}
+            alt="logo"
+        />
+        <button
+            onClick={() => signIn("google")}
+            className=" text-black font-bold text-2xl animate-pulse border border-black border-solid rounded-lg py-2 px-4">
+            Sign In to ChatGPT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
